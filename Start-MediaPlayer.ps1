123d952478a6f8f1f6535a59bd34e1af8ec266f6
Prateek Singh <Prateeksingh1590@gmail.com>
2016-07-19 14:55:15 +0530

    Update Start-MediaPlayer.ps1

--- Start-MediaPlayer.ps1
+++ Start-MediaPlayer.ps1
@@ -1,20 +1,72 @@
 <#
-    Function to play songs in Windows media player from Powershell Console, change music tracks, shuffle, Stop.
+.Synopsis
+   Plays music on Windows Media PLayer in backgroud.
+.DESCRIPTION
+   Function automates Windows Media Player to play songs in Background in order (Random/Sequential) chosen by the user.
+   Moreover, It generates a balloon notification in bottom Right corner of the screen, whenever a new song starts playing and continues to do that until manually stopped or it completes playing all songs.
+.PARAMETER Path
+    Music Directory [ Example - Music -Path C:\music\ ]
+.PARAMETER Shuffle
+    Switch to play music in shuffle mode, default value is 'sequential'
+.PARAMETER Loop
+    Switch to continuously play songs in a infinite Loop.
+.PARAMETER Stop
+    Switch to kill any instance of Music playing in backgroud.
+.EXAMPLE
+    PS Root\> Music F:\Data\Music\MyPlaylist
+    TotalSongs TotalPlayDuration Mode      
+    ---------- ----------------- ----      
+    26         98 Mins           Sequential 
+   
+    Example shows how to run music from by passing a music directory to the Function
+.EXAMPLE
+    PS Root\> Music -Verbose
+    [VERBOSE] You've not provided a music directory, looking for cached information from Previous use.
+    [VERBOSE] Starting a background Job to play Music files
+    
+    TotalSongs TotalPlayDuration Mode      
+    ---------- ----------------- ----      
+    26         98 Mins           Sequential
 
-    From : Prateek Singh - @SinghPrateik
-#> 
+    Example shows that in case you don't provide a music directory, the function Looks for the cached information of the diretory from previous us of the function.
+    Moreover, It displays the information like TotalSongs, Total play duration, and Mode chosen by the user
+.EXAMPLE
+    PS Root\> Music -Shuffle
+    TotalSongs TotalPlayDuration Mode   
+    ---------- ----------------- ----   
+    26         98 Mins           Shuffle
+
+    Choose 'Shuffle' switch to play music in shuffle mode, default value is 'sequential'.
+.EXAMPLE
+    PS Root\> Music -Shuffle -Loop
+    TotalSongs TotalPlayDuration Mode           
+    ---------- ----------------- ----           
+    26         Infinite Mins     Shuffle in Loop 
+
+    Choose 'Loop' switch inorder to continuously play songs in a infinite Loop.
+.EXAMPLE
+    PS Root\> Music -Stop -Verbose
+    [VERBOSE] Stoping any Already running instance of Media in background.
+
+    When 'Stop' switch is used any instance Music playing in backgroud stops.
+.NOTES
+   Author Name    : Prateek Singh
+   Twitter Handle : @SinghPrateik
+   Blog           : http://geekeefy.wordpress.com
+#>
 Function Start-MediaPlayer
 {
     [cmdletbinding()]
     Param(
-            [Alias('P')][String] $Path,
-            [Alias('Sh')][switch] $Shuffle,
-            [Alias('St')][Switch] $Stop,
-            [Alias('L')][Switch] $Loop
+            [Alias('P')]  [String] $Path,
+            [Alias('Sh')] [switch] $Shuffle,
+            [Alias('St')] [Switch] $Stop,
+            [Alias('L')]  [Switch] $Loop
     )
 
     If($Stop)
     {
+        Write-Verbose "Stoping any Already running instance of Media in background."
         Get-Job MusicPlayer -ErrorAction SilentlyContinue | Remove-Job -Force
     }
     Else
@@ -28,16 +80,19 @@ Function Start-MediaPlayer
         {
             If((cat C:\Temp\Musicplayer.txt -ErrorAction SilentlyContinue).Length -ne 0)
             {
+                Write-Verbose "You've not provided a music directory, looking for cached information from Previous use."
                 $path = cat C:\Temp\Musicplayer.txt
 
                 If(-not (Test-Path $Path))
                 {
-                    "Please provide a path to music directory."       
+                    "Please provide a path to a music directory.`nFound a cached directory `"$Path`" from previous use, but that too isn't accessible!"
+                    # Mark Path as Empty string, If Cached path doesn't exist
+                    $Path = ''     
                 }
             }
             else
             {
-                "Please provide a path to music directory."
+                "Please provide a path to a music directory."
             }
         }
  
@@ -76,11 +131,12 @@ Function Start-MediaPlayer
     	                # Instantiate Media Player Class
     	                $MediaPlayer = New-Object System.Windows.Media.Mediaplayer
                         
-                        # Crunching the numbers and Info
-                        $FileList = gci $Path -Recurse -Include *.mp3 | select fullname, @{n='Duration';e={get-songduration $_.fullname}}
+                        # Crunching the numbers and Information
+                        $FileList = gci $Path -Recurse -Include *.mp* | select fullname, @{n='Duration';e={get-songduration $_.fullname}}
                         $FileCount = $FileList.count
                         $TotalPlayDuration =  [Math]::Round(($FileList.duration | measure -Sum).sum /60)
                         
+                        # Condition to identifed the Mode chosed by the user
                         if($Shuffle)
                         {
                             $Mode = "Shuffle"
@@ -91,13 +147,21 @@ Function Start-MediaPlayer
                             $Mode = "Sequential"
                         }
                         
+                        # Check If user chose to play songs in Loop
                         If($Loop)
                         {
                             $Mode = $Mode + " in Loop"
                             $TotalPlayDuration = "Infinite"
                         }
-                            
-    	                ''| select @{n='TotalSongs';e={$FileCount};},@{n='PlayDuration';e={[String]$TotalPlayDuration + " Mins"}},@{n='Mode';e={$Mode}} 
+                        
+                        If($FileList)
+                        {    
+    	                    ''| select @{n='TotalSongs';e={$FileCount};},@{n='PlayDuration';e={[String]$TotalPlayDuration + " Mins"}},@{n='Mode';e={$Mode}} 
+                        }
+                        else
+                        {
+                            "No music files found in directory `"$path`" ." 
+                        }
                         
                         Do
                         {
@@ -109,22 +173,26 @@ Function Start-MediaPlayer
 		                    		        Show-NotifyBalloon ($Message)                   # 3. Show a notification balloon in system tray
 		                    		        Start-Sleep -Seconds $_.duration                # 4. Pause the script execution until song completes
 		                    		        $MediaPlayer.Stop()                             # 5. Stop the Song
-		                    		        $Balloon.Dispose()                           
+		                    		        $Balloon.Dispose();$Balloon.visible =$false                           
 	                        }
-                        }While($Loop)
+                        }While($Loop) # Play Infinitely If 'Loop' is chosen by user
                     }
         }
 
-        #Removes any already running Job, and start a new job, that looks like changing the track
-
+        # Removes any already running Job, and start a new job, that looks like changing the track
         If($(Get-Job Musicplayer -ErrorAction SilentlyContinue))
         {
             Get-Job MusicPlayer -ErrorAction SilentlyContinue |Remove-Job -Force
         }
 
-        Start-Job -Name MusicPlayer -InitializationScript $init -ScriptBlock {playmusic $args[0] $args[1] $args[2]} -ArgumentList $path, $Shuffle, $Loop | Out-Null
-        Start-Sleep -s 3 # Sleep to allow media player some breathing time to load files
-        Receive-Job -Name MusicPlayer | ft @{n='TotalSongs';e={$_.TotalSongs};alignment='left'},@{n='TotalPlayDuration';e={$_.PlayDuration};alignment='left'},@{n='Mode';e={$_.Mode};alignment='left'} -AutoSize
+        # Run only if path was Defined or retrieved from cached information
+        If($Path)
+        {
+            Write-Verbose "Starting a background Job to play Music files"
+            Start-Job -Name MusicPlayer -InitializationScript $init -ScriptBlock {playmusic $args[0] $args[1] $args[2]} -ArgumentList $path, $Shuffle, $Loop | Out-Null
+            Start-Sleep -Seconds 3       # Sleep to allow media player some breathing time to load files
+            Receive-Job -Name MusicPlayer | ft @{n='TotalSongs';e={$_.TotalSongs};alignment='left'},@{n='TotalPlayDuration';e={$_.PlayDuration};alignment='left'},@{n='Mode';e={$_.Mode};alignment='left'} -AutoSize
+        }
  }
     
 }
